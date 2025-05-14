d704bf49c6dc15b87362ee5620c6d30da81c6def
Alejandro Bufarini <alejandroebufarini@gmail.com>
2025-05-14 15:20:19 -0300

    🔧 Update dependencies and enhance TestRunner configuration: add JUnit 4.13.2 for testing and configure Serenity reporter

--- src/test/java/starter/runner/TestRunner.java
+++ src/test/java/starter/runner/TestRunner.java
@@ -5,12 +5,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "starter.steps,starter.hooks")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/site/serenity/serenity.json")
-public class TestRunner {
-}
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, io.cucumber.core.plugin.SerenityReporterParallel")
+public class TestRunner {}
