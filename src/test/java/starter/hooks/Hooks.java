0ba289b4ab80600fe8f3d59e7d8d98d676047859
Alejandro Bufarini <alejandroebufarini@gmail.com>
2025-05-14 13:56:49 -0300

    🧹 Remove screenshot capture from Hooks and centralize in ValidationUtils

--- src/test/java/starter/hooks/Hooks.java
+++ src/test/java/starter/hooks/Hooks.java
@@ -1,35 +1,27 @@
 package starter.hooks;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import starter.core.StepContext;
+import starter.core.TestContext;
 import starter.core.driver.DriverProvider;
 
 public class Hooks {
 
     @Before
-    public void beforeScenario() {
+    public void beforeScenario(Scenario scenario) {
+        StepContext.setScenario(scenario);
         OnStage.setTheStage(new OnlineCast()); // Screenplay stage
         DriverProvider.initializeDriver();      // Driver por thread
     }
 
     @After
     public void afterScenario() {
+        TestContext.clear();
         DriverProvider.quitDriver();           // Cierra el driver de ese thread
-    }
-
-    @After
-    public void takeScreenshotOnFailure(Scenario scenario) {
-        if (scenario.isFailed()) {
-            byte[] screenshot = ((TakesScreenshot) DriverProvider.getDriver())
-                    .getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
-        }
-        DriverProvider.quitDriver();
     }
 
 }
