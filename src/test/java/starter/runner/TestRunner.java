dcc8601ccb6cae68b0d268e079718c389ca4a378
Alejandro Bufarini <alejandroebufarini@gmail.com>
2025-05-14 15:13:24 -0300

    🔧 Update project configuration: add JUnit 5 dependency, configure Serenity output, and enhance TestRunner for JSON reporting

--- src/test/java/starter/runner/TestRunner.java
+++ src/test/java/starter/runner/TestRunner.java
@@ -1,16 +1,16 @@
 package starter.runner;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.ConfigurationParameter;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "starter.steps,starter.hooks")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-public class TestRunner {}
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/site/serenity/serenity.json")
+public class TestRunner {
+}
